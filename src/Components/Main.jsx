4be5c0039c8208820a5eaac0dab4e25cb682da28
TahaSyedKhan <tahasyedkhan123@gmail.com>
2025-08-20 16:12:04 +0530

    added some basic functionality

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,29 +1,68 @@
+import { useState } from "react"
 import "../App.css"
 export default function Main() {
+
+    const [formData, setFormData] = useState({ language: "french", text: ""})
+    const [error, setError] = useState("")
+
+    const handleInputChange = (e) => { 
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+        console.log(formData)
+        setError("")
+    }
+
+    const translate = () => {}
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        if(!formData.text) {
+            setError("Please enter the text.")
+            return
+        }
+        translate()
+    }
     return (
         <main>
-            <form>
-        <h2 class="section-title">Text to translate <span>👇</span></h2>
-        <textarea placeholder="How are you?"></textarea>
+            <form onSubmit={handleOnSubmit}>
+                <h2 class="section-title">Text to translate <span>👇</span></h2>
 
-        <h2 class="section-title">Select language <span>👇</span></h2>
-        <div class="choices">
-            <label>
-                <input type="radio" name="language" value="french"/>
-                French 🇫🇷
-            </label>
-            <label>
-                <input type="radio" name="language" value="spanish" />
-                Spanish 🇪🇸
-            </label>
-            <label>
-                <input type="radio" name="language" value="japanese" />
-                Japanese 🇯🇵
-            </label>
-        </div>
+                <textarea 
+                    placeholder="How are you?" 
+                    name="text" 
+                    onChange={handleInputChange}>
+                </textarea>
 
-        <button type="submit">Translate</button>
-    </form>
+                <h2 class="section-title">Select language <span>👇</span></h2>
+                <div class="choices">
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="language" 
+                            value="french" 
+                            defaultChecked={formData.language} 
+                            onChange={handleInputChange}/>
+                        French 🇫🇷
+                    </label>
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="language" 
+                            value="spanish" 
+                            onChange={handleInputChange}/>
+                        Spanish 🇪🇸
+                    </label>
+                    <label>
+                        <input 
+                            type="radio" 
+                            name="language" 
+                            value="japanese" 
+                            onChange={handleInputChange}/>
+                        Japanese 🇯🇵
+                    </label>
+                </div>
+                {error && <div>{error}</div>}
+                <button type="submit">Translate</button>
+            </form>
         </main>
     )
 }
